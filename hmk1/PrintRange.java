068cfba2563551bd88391e77febc0385759aa962
Otmanetildi <Otmane18fnqouche@gmail.com>
2026-01-01 20:44:32 +0300

    satisfying requirement change three

--- hmk1/PrintRange.java
+++ hmk1/PrintRange.java
@@ -13,14 +13,19 @@ public class PrintRange {
         int b = scanner.nextInt();
 
         if (a < b) {
-            for (int i = a + 1; i < b; i++) {
-                System.out.println(i);
-            }
-        } else {
-            for (int i = b + 1; i < a; i++) {
-                System.out.println(i);
-            }
-        }
+    		for (int i = a + 1; i < b; i++) {
+        		if (i % 2 == 0) {
+            			System.out.println(i);
+       			}
+    		}
+	} else {
+    		for (int i = b + 1; i < a; i++) {
+        		if (i % 2 == 0) {
+            			System.out.println(i);
+        		}
+    		}
+	}
+
 
         scanner.close();
 
